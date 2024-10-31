ff60cf8d5e8f55831f5429abbe1b0d9a648ac8d5
Rihan Badhon <149458226+RihanBadhon@users.noreply.github.com>
2024-10-31 07:28:56 -0700

    Update script.js

--- src/script/script.js
+++ src/script/script.js
@@ -1,15 +1,12 @@
 'use strict';
-
 const alarmSound = new Audio('./src/audio/Buzzer sound.wav');
 const alarmTimeInput = document.getElementById('alarm-time');
 const setAlarmButton = document.getElementById('set-alarm-btn');
 const alarmStatus = document.getElementById('alarm-status');
 const hoursElement = document.getElementById("hours");
 const minutesElement = document.getElementById("minutes");
-
 let alarmTime = null;
 let alarmIntervalId = null;
-
 function updateClock() {
   const now = new Date();
   const hours = now.getHours().toString().padStart(2, '0');
@@ -17,25 +14,19 @@ function updateClock() {
   hoursElement.textContent = hours;
   minutesElement.textContent = minutes;
 }
-
 function startAlarm() {
   alarmStatus.textContent = 'Alarm is Set for ' + alarmTime + '!';
   setAlarmButton.disabled = true;
-
-
   alarmIntervalId = setInterval(() => {
     const now = new Date();
     const hours = now.getHours().toString().padStart(2, '0');
     const minutes = now.getMinutes().toString().padStart(2, '0');
     const currentTime = hours + ':' + minutes;
-
-
     if (alarmTime === currentTime) {
       alarmSound.play();
       clearInterval(alarmIntervalId);
       alarmStatus.textContent = 'Alarm ringing!';
       setAlarmButton.disabled = false;
-
     // The botton line below trigger to  Stop the sound after 60 seconds
     // and reset the status message
       setTimeout(() => {                  
@@ -46,7 +37,6 @@ function startAlarm() {
     }
   }, 1000);
 }
-
 function setAlarm() {
   const regExp = /^([01]\d|2[0-3]):([0-5]\d)$/;   
   // this function above validate or check 
@@ -58,8 +48,6 @@ function setAlarm() {
     } else {
     alarmStatus.innerText = 'Invalid Input, Enter The Time in HH:MM Format.';
     }
-
 }
-
 setInterval(updateClock, 1000);
 setAlarmButton.addEventListener('click', setAlarm);
